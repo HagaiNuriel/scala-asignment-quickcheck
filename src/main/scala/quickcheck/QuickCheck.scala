f9358235f32f5c68bdcc3ec06ad112b8034c8d01
hagainuriel <hagain@salt.security>
2023-05-22 17:57:56 +0300

    quickCheck assignment

---
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,23 @@
+package quickcheck
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop.forAll
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      k <- arbitrary[A]
+      m <- oneOf(const(empty), genHeap)
+    } yield insert(k, m)
+  )
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+}
